103cb7d9f77d1038cc84a97c79d802d9b5405d2b
Tyler Koistinen <koistinen.tyler@gmail.com>
2020-08-25 10:55:43 -0400

    Misc tweaks I don't remember

--- backend/src/main/java/com/tutorial/backend/GameController.java
+++ backend/src/main/java/com/tutorial/backend/GameController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @CrossOrigin
 public class GameController {
